006991206e84f545720f6daa059687d7cd48c3cc
Sourav <sapnandu@gmail.com>
2023-03-17 14:54:08 +0530

    Create lead

---
+++ EquitasInboundAPI/Controllers/CaseController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace EquitasInboundAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CaseController : ControllerBase
+    {
+
+        private readonly ILogger<LeadController> _log;
+        private readonly IQueryParser _queryp;
+
+        public CaseController(ILogger<LeadController> log, IQueryParser queryParser)
+        {
+            this._log = log;
+            this._queryp = queryParser;
+        }
+
+
+        [HttpPost("CreateCase")]        
+        public async Task<IActionResult> CreateCase()
+        {
+            try
+            {
+                StreamReader requestReader = new StreamReader(Request.Body);
+                dynamic request = JObject.Parse(await requestReader.ReadToEndAsync());
+                CreateLeadExecution createleadEx = new CreateLeadExecution(this._log, this._queryp);
+                LeadReturnParam Leadstatus = await createleadEx.ValidateLeadeStatus(request);
+                return Ok(Leadstatus);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+
+            }
+
+        }
+    }
+}
